ba650851943758acc4668e70aec267d3effe6a72
Orca <wbjung2917@gmail.com>
2022-11-09 13:18:52 +0900

    update check code for checkedItems

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
 import { useWord } from '../hooks/word-context';
 
 export const Test = () => {
   const { checkedItems } = useWord();
 
+  console.log(checkedItems);
   return <h1>this is testpage</h1>;
 };
