39c45994f8d274ae7e99698f225b89da6d05d532
Orca <wbjung2917@gmail.com>
2022-11-09 09:01:09 +0900

    add delete word buttons

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,8 +1,14 @@
+import { XCircleIcon } from '@heroicons/react/24/outline';
 export const Word = ({ en, ko }) => {
   return (
     <div className='mx-2 flex-col text-center'>
       <h1 className='text-xl font-bold text-sky-500'>{en}</h1>
-      <h1 className='text-lg font-bold text-sky-700'>{ko}</h1>
+      <div className='flex items-center justify-center'>
+        <h1 className='mr-2 text-lg font-bold text-sky-700'>{ko}</h1>
+        <button className='w-6 text-rose-500'>
+          <XCircleIcon />
+        </button>
+      </div>
     </div>
   );
 };
